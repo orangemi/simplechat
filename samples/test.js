0afdce65178ef061d054e1b5057329fe855c2dea
Orange Mi <orgmi@sina.com>
2014-05-11 01:03:54 +0800

    add package.json

--- samples/test.js
+++ samples/test.js
@@ -1,17 +1,4 @@
-var Cache = require('../lib/Cache');
-
-var c = new Cache();
-c.save('1','abc',1);
-console.log(c.data);
-// var ioServer = require('socket.io');
-
-// var server = new ioServer();
-
-// server.listen(3001).on('connection', function(socket) {
-// 	console.log('shakehand...');
-// 	socket.emit('message', {data:1}, function(code) {
-// 		console.log(code);
-// 	});
-// });
-
-// console.log('listen 3001...');
+var Log = require('../lib/Log');
+var logger = new Log();
+//console.log(logger.dir);
+logger.log('abc');
